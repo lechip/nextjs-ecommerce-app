1c84118a971bded6729935a008a9d76721f99c0d
lechip <lechip@gmail.com>
2020-09-06 15:48:24 +0200

    Addig my own test API key

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Index = (props: IIndexProps) => {
     <div className="app">
       <Head>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-        <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="MzMxN2Y0ODMtOWNhMy00YzUzLWFiNTYtZjMwZTRkZDcxYzM4" id="snipcart"></script>
+        <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="N2U4NmNhNmEtNjhmZi00OTFlLTlmNDMtZWQ1MDI3MTVlNDU0NjM3MzQ5NzUwMjMwMDM5ODQw" id="snipcart"></script>
         <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
         <link rel="shortcut icon" href="/static/favicon.ico" />
       </Head>
